0f7995f2efd090e7b1abb64649927961ec48ce5e
AHMED LOTFI <ahmlot92@gmail.com>
2023-04-20 10:31:25 -0700

    tasks 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * print_name - prints a name as function pointer
- * @name: name to add 
- * @f: point to fun
- * Return: Nothing
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: point to funct
+ * Return: nothing
  **/
 void print_name(char *name, void (*f)(char *))
 {
