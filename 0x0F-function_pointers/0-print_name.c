3aa786c71fe0bae3948a04e08f12fe8e60caa419
AHMED LOTFI <ahmlot92@gmail.com>
2023-04-20 10:20:39 -0700

    tasks 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,10 +6,10 @@
  * @f: point to fun
  * Return: Nothing
  */
-void print_name(char *name, void (*f)(char *))
-{
-	if (name == NULL || f == NULL)
-               return;  
+void print_name(char *name, void (*f)(char *)){i
 
-		       f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
