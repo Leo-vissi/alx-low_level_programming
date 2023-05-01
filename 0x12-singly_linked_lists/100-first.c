9d50f808757fb48a5594bf8caa9f16ab828d8cc3
ahmed <ahmlot92@gmail.com>
2023-05-01 06:31:55 -0700

    tasks adv

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * main - prints a message before main
  */
 
-void main(void)
+void __attribute__((constructor)) main()
 {
     printf("You're beat! and yet, you must allow,\n");
     printf("I bore my house upon my back!\n");
