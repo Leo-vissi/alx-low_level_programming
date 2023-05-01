20f03e9f00152943b7712d208cdbbf522524e853
ahmed <ahmlot92@gmail.com>
2023-05-01 05:40:43 -0700

    tasks 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t linked list
+ * @head: pointer to the head of the list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+        list_t *vari = head;
+
+        while (head != NULL)
+        {
+                vari = head->next;
+                free(head->str);
+                free(head);
+                head = vari;
+        }
+}
