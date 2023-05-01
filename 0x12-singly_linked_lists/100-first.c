7f6043db8cdd26b6bde82a507607ad8e52581abf
ahmed <ahmlot92@gmail.com>
2023-05-01 06:25:36 -0700

    tasks adv

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * _main_ - prints a message before main 
+ */
+
+void _main_(void)
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
