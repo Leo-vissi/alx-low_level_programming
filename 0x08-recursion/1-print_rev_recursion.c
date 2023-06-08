0eaa906ebd8b10ba550616754b18ad0d917c58c1
ahmed sayf <ahmlot92@gmail.com>
2023-06-08 07:37:38 -0700

    tasks 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: Str to print
+ */
+
+void _print_rev_recursion(char *s);
+{
+	    if (*s)
+    {
+	     _print_rev_recursion(s + 1);
+             _printchar(*s);
+
+        return;
+    }
+}
