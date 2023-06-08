f8db8a7bb81165020b44600952907a6279102fa9
ahmed sayf <ahmlot92@gmail.com>
2023-06-08 07:14:50 -0700

    tasks 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,15 +7,15 @@
  * Return: Always 0 (Succ)
  */
 
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s == '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);
-		return;
+		s++;
+		_puts_recursion(s);
 	}
 
 	else
-	        _putchar('\n');
+		_putchar('\n');
 }
