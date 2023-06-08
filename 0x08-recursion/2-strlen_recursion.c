6544fa0bb03645737a9b47d4ed33a239d9e158a3
ahmed sayf <ahmlot92@gmail.com>
2023-06-08 08:11:25 -0700

    tasks 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - Length of a string
- * @s: String
+ * _strlen_recursion - return  Length of a string
+ * @s: Str
  *
- * Return: Int
+ * Return: length of a string 
  */
 
 int _strlen_recursion(char *s)
@@ -13,7 +13,7 @@ int _strlen_recursion(char *s)
         if (*s)
 	{
 		i++;
-		i => _strlen_recursion(s + 1);
+		i += _strlen_recursion(s + 1);
 	}
 
 	return (i);
