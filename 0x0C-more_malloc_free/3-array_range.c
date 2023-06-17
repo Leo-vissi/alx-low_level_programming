87fbf7cd76a5434bb62d4f76f49d98bf742e80fd
ahmed sayf <ahmlot92@gmail.com>
2023-06-16 17:51:11 -0700

    tasks

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -7,23 +7,22 @@
  * @min: The minimum value of the range.
  *
  * Return: Pointer to the created array.
- * 
  */
 
 int *array_range(int min, int max)
 {
-	int *array x = 0, y = min;
+	int *ar_rang, x = 0, y = min;
 
 	if (min > max)
 		return (NULL);
 
-	array = malloc((max - min + 1) * sizeof(int));
+	ar_rang = malloc((max - min + 1) * sizeof(int));
 
-	if (!array)
+	if (!ar_rang)
 		return (NULL);
 
 	while (x <= max - min)
-		array[x++] = y++;
+		ar_rang[x++] = y++;
 
-	return (array);
+	return (ar_rang);
 }
